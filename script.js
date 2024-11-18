795b7596bf3a5a31af11db097597b1fc3ee7beff
juned072 <junedansari9172@gmail.com>
2024-11-18 12:10:07 +0530

    Implemented Functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+const searchField = document.getElementById("search");
+const feedBack = document.getElementById("feedBack");
+let guessAttempt = 0;
+
+function guessNumber() {
+  const userGuess = parseInt(searchField.value);
+
+  const computerGuess = Math.floor(Math.random() * 2) + 1;
+  if (isNaN(userGuess) || !userGuess) {
+    feedBack.textContent = "Please enter valid number!";
+    return;
+  }
+  guessAttempt++;
+
+  if (userGuess === computerGuess) {
+    feedBack.textContent = `🎉Congratulation, You Won this game in ${guessAttempt} Attempts`;
+    guessAttempt = 0;
+  } else if (userGuess < computerGuess) {
+    feedBack.textContent = "You guess to low , Try again!";
+  } else {
+    feedBack.textContent = "You guess to high , Try again!";
+  }
+
+  searchField.value = "";
+}
+
+function restartGame() {}
